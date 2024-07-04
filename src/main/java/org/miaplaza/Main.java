8f346ea663775bcf4c526453faa3ca8574aa4edb
MalikSanaChohan <malikanachochan@gmail.com>
2024-07-04 14:51:54 -0700

    Package renamed

--- src/main/java/org/miaplaza/Main.java
+++ src/main/java/org/miaplaza/Main.java
@@ -12,12 +12,12 @@ public class Main {
         driver.get("https://miacademy.co/#/");
         driver.findElement(By.xpath("//a[@href='https://miaprep.com/online-school/']")).click();
         driver.findElement(By.xpath("//div[@class='wp-block-button']//a")).click();
-        driver.findElement(By.xpath("//div[@class='flag-container']")).click();
-        driver.findElement(By.xpath("//ul[contains(@class,'country-list')]//li[@data-dial-code='92']")).click();
-        driver.findElement(By.xpath("//input[@name='PhoneNumber']")).sendKeys("3026569857");
         driver.findElement(By.xpath("//input[@elname='First' and @name='Name']")).sendKeys("Ali");
         driver.findElement(By.xpath("//input[@elname='Last' and @name='Name']")).sendKeys("Malik");
         driver.findElement(By.id("Email-arialabel")).sendKeys("sanamalik@gmail.com");
+        driver.findElement(By.xpath("//div[@class='flag-container']")).click();
+        driver.findElement(By.xpath("//ul[contains(@class,'country-list')]//li[@data-dial-code='92']")).click();
+        driver.findElement(By.xpath("//input[@name='PhoneNumber']")).sendKeys("3026569857");
         driver.findElement(By.xpath("//span[@id= 'select2-Dropdown-arialabel-container']//div")).click();
         driver.findElement(By.xpath("//li[text()='Yes']")).click();
         driver.findElement(By.xpath("//label[@for ='Checkbox_4']")).click();
