43a88ed9558287f3331990af8235290101c8bdd0
MalikSanaChohan <malikanachochan@gmail.com>
2024-07-04 14:59:34 -0700

    Package renamed

--- src/main/java/org/miaplaza/Main.java
+++ src/main/java/org/miaplaza/Main.java
@@ -7,8 +7,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
     static WebDriver driver = new ChromeDriver();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
         driver.get("https://miacademy.co/#/");
         driver.findElement(By.xpath("//a[@href='https://miaprep.com/online-school/']")).click();
         driver.findElement(By.xpath("//div[@class='wp-block-button']//a")).click();
@@ -19,7 +21,7 @@ public class Main {
         driver.findElement(By.xpath("//ul[contains(@class,'country-list')]//li[@data-dial-code='92']")).click();
         driver.findElement(By.xpath("//input[@name='PhoneNumber']")).sendKeys("3026569857");
         driver.findElement(By.xpath("//span[@id= 'select2-Dropdown-arialabel-container']//div")).click();
-        driver.findElement(By.xpath("//li[text()='Yes']")).click();
+        driver.findElement(By.xpath("//li[text()='No']")).click();
         driver.findElement(By.xpath("//label[@for ='Checkbox_4']")).click();
         driver.findElement(By.xpath("//label[@for ='Checkbox_2']")).click();
         driver.findElement(By.id("Date-date")).click();
